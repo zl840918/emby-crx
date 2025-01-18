3428fd3ce88e1140a7334b14d4b86b175645c523
zl840918 <54974261+zl840918@users.noreply.github.com>
2025-01-18 15:52:21 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,11 @@
 # 创建emby-crx目录并下载所需文件
 rm -rf emby-crx
 mkdir -p emby-crx
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/css/style.css -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/common-utils.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/static/js/md5.min.js -P emby-crx/
-wget https://raw.githubusercontent.com/Nolovenodie/emby-crx/master/content/main.js -P emby-crx/
+wget https://raw.githubusercontent.com/zl840918/emby-crx/master/static/css/style.css -P emby-crx/
+wget https://raw.githubusercontent.com/zl840918/emby-crx/master/static/js/common-utils.js -P emby-crx/
+wget https://raw.githubusercontent.com/zl840918/emby-crx/master/static/js/jquery-3.6.0.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/zl840918/emby-crx/master/static/js/md5.min.js -P emby-crx/
+wget https://raw.githubusercontent.com/zl840918/emby-crx/master/content/main.js -P emby-crx/
 
 # 读取index.html文件内容
 content=$(cat index.html)
